873f334cb889c84d9385cd8244ee4a00b6f0e655
JouiniEya-dev <jouiniieya6@gmail.com>
2025-03-29 22:52:09 +0100

    Ajout de la gestion des tâches dans le dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
 import { TASK_STATUTS_CLASS_MAP, TASK_STATUTS_TEXT_MAP } from './constants';
 
 export default function Dashboard({ 
@@ -86,8 +86,15 @@ export default function Dashboard({
                         {activeTasks.data.map((task) => (
                         <tr key={task.id}>
                             <td className='px-3 py-2'>{task.id}</td>
-                            <td className='px-3 py-2'>{task.project.name}</td>
-                            <td className='px-3 py-2'>{task.name}</td>
+                            <td className='px-3 py-2 text-white hover:underline'>
+                                <Link href={route('project.show', task.project.id)}>
+                                {task.project.name}
+                                </Link>
+                            </td>
+                            <td className='px-3 py-2 text-white hover:underline'> 
+                                <Link href={route('task.show', task.id)}>
+                                {task.name}
+                                </Link></td>
                             <td className='px-3 py-2'>
                                 <span className={`px-2 py-1 rounded text-nowrap text-white ${TASK_STATUTS_CLASS_MAP[task.status] || 'bg-gray-500'}`}>
                                     {TASK_STATUTS_TEXT_MAP[task.status]}
